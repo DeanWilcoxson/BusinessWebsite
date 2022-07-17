d147d863049a9177dffc89d86b2fc7614f179836
Dean Wilcoxson <dmwhswsmw919216@gmail.com>
2022-07-16 22:32:21 -0700

    7/16/22

--- Frontend/src/Components/Layout/Topbar/Logo/LogoElements.js
+++ Frontend/src/Components/Layout/Topbar/Logo/LogoElements.js
@@ -2,3 +2,4 @@ import styled from "styled-components;";
 import BusinessLogo from "../../../../images/BusinessLogo";
 export const LogoContainer = styled.div``;
 export const LogoImg = styled(BusinessLogo)``;
+export const BusinessName = styled.h1``
--- Frontend/src/Components/Layout/Topbar/Logo/index.js
+++ Frontend/src/Components/Layout/Topbar/Logo/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { LogoContainer, LogoImg } from "./LogoElements";
+import { LogoContainer, LogoImg, BusinessName } from "./LogoElements";
 const Logo = () => {
   return (
     <LogoContainer>
       <LogoImg></LogoImg>
+      <BusinessName>DB Web Development LLC</BusinessName>
     </LogoContainer>
   );
 };
