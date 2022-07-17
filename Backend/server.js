208a64244e9b564172f62172a22274c62af7f792
bryanhuffstutler <bhuff8404@gmail.com>
2022-07-17 16:34:17 -0500

    setting up backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,2 +1,20 @@
-const Express = require('express');
+const Express = require("express");
 const app = express();
+const morgan = require("morgan");
+import dbConnect from "./helpers/dbConnect";
+
+app.use(morgan("dev"));
+
+dbConnect();
+
+app.use("/auth", require("./routes/authRouter.js"));
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  if (err.name === "Unauthorized Error") {
+    res.status(err.status);
+  }
+  return res.send({ errMsg: err.message });
+});
+
+app.listen(9000, () => {console.log("Server is running on Port: 9000")})
