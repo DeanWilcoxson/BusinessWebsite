0027c8f87df45c417eaf32c4b0bf1d96b9c591b1
bryanhuffstutler <bhuff8404@gmail.com>
2022-07-24 21:12:02 -0500

    auth route

--- Frontend/src/Components/Auth/AuthElements.js
+++ Frontend/src/Components/Auth/AuthElements.js
@@ -3,4 +3,4 @@ export const AuthContainer = styled.div``;
 export const AuthBox = styled.div``;
 export const AuthForm = styled.form``;
 export const AuthInput = styled.input``;
-export const AuthButton = styled.button;
+export const AuthButton = styled.button``;
