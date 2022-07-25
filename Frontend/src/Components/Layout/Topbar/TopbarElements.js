789b06b542cb4dd3ac383c7a8ce253089b3a238f
Dean Wilcoxson <dmwhswsmw919216@gmail.com>
2022-07-25 14:22:40 -0700

    Finished Topbar

--- Frontend/src/Components/Layout/Topbar/TopbarElements.js
+++ Frontend/src/Components/Layout/Topbar/TopbarElements.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 export const TopbarContainer = styled.div`
   margin: 0;
   padding: 0;
-  background: linear-gradient( to right, #000000, black, grey, darkgrey);
+  background: linear-gradient(to right, #000000, black, #888888, grey);
 `;
 export const TopbarBox = styled.div`
   display: flex;
