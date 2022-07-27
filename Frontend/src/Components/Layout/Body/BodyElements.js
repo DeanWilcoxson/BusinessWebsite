11853f1b020140f8bfe7ee9c696658b7274f0a95
Dean Wilcoxson <dmwhswsmw919216@gmail.com>
2022-07-27 16:47:19 -0700

    Body Folder Object setup

---
+++ Frontend/src/Components/Layout/Body/BodyElements.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+export const BodyContainer = styled.div``;
+export const BodyBox = styled.div``;
+export const BodyButton = styled.button``;
+export const BodyDesc = styled.p``;
