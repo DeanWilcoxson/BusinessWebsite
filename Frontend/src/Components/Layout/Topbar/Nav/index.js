55c137b8f004453fa6d9952d26421fe4129a544b
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2022-08-16 11:19:13 -0700

    8/16/22

--- Frontend/src/Components/Layout/Topbar/Nav/index.js
+++ Frontend/src/Components/Layout/Topbar/Nav/index.js
@@ -10,8 +10,15 @@ import {
 } from "./NavElements";
 const Nav = () => {
   const [homeToggle, setHomeToggle] = useState(false);
+  const [packagesToggle, setPackagesToggle] = useState(false);
 
-  function handleToggle() {
+  function handleHomeToggle() {
+    setHomeToggle(!homeToggle);
+  }
+  function handlePackagesToggle() {
+    setPackagesToggle(!packagesToggle);
+  }
+  function handleHomeToggle() {
     setHomeToggle(!homeToggle);
   }
 
@@ -19,7 +26,7 @@ const Nav = () => {
     <NavContainer>
       <NavBox>
         <NavDropDown>
-          <NavRouterButton to="/" onClick={handleToggle}>
+          <NavRouterButton to="/" onClick={handleHomeToggle}>
             Home<NavDropDownCaret></NavDropDownCaret>
           </NavRouterButton>
           {homeToggle ? (
@@ -53,7 +60,39 @@ const Nav = () => {
             ""
           )}
         </NavDropDown>
-        <NavRouterButton to="/packages">Packages</NavRouterButton>
+        <NavDropDown>
+          <NavRouterButton to="/packages">Packages</NavRouterButton>
+          {packagesToggle ? (
+            <NavDropDownContainer>
+              <NavScrollButton
+                to="overview"
+                spy={true}
+                smooth={true}
+                onClick={handleToggle}
+              >
+                Overview
+              </NavScrollButton>
+              <NavScrollButton
+                to="technology"
+                spy={true}
+                smooth={true}
+                onClick={handleToggle}
+              >
+                Technology
+              </NavScrollButton>
+              <NavScrollButton
+                to="hosting"
+                spy={true}
+                smooth={true}
+                onClick={handleToggle}
+              >
+                Hosting
+              </NavScrollButton>
+            </NavDropDownContainer>
+          ) : (
+            ""
+          )}
+        </NavDropDown>
         <NavRouterButton to="/templates">Templates</NavRouterButton>
         <NavRouterButton to="/about">About</NavRouterButton>
       </NavBox>
